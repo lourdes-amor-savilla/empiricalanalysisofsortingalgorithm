9ba1918a2ce5ecd64a82fb515a215ede09087f09
Cloud Daet <cloudsedgwickbalute.daet24@bicol-u.edu.ph>
2026-04-12 19:06:04 +0800

    Refactor quicksort with median of three partitioning

--- sorting.c
+++ sorting.c
@@ -119,30 +119,56 @@ void mergeSort(int arr[], int left, int right) {
 // QUICK SORT
 //===========================================================================================
 
-int partition(int arr[], int low, int high) {
-    int pivot = arr[high];
-    int i = (low - 1);
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    for (int j = low; j <= high - 1; j++) {
-        if (arr[j] <= pivot) {
+int medianOfThree(int arr[], int low, int high) {
+    int mid = low + (high - low) / 2;
+
+    if (arr[low] > arr[mid])
+        swap(&arr[low], &arr[mid]);
+
+    if (arr[low] > arr[high])
+        swap(&arr[low], &arr[high]);
+
+    if (arr[mid] > arr[high])
+        swap(&arr[mid], &arr[high]);
+
+    return mid;  // median value index
+}
+
+int partition(int arr[], int low, int high) {
+    int pivotIndex = medianOfThree(arr, low, high);
+    int pivot = arr[pivotIndex];
+
+    int i = low - 1;
+    int j = high + 1;
+
+    while (1) {
+        do {
             i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
+        } while (arr[i] < pivot);
+
+        do {
+            j--;
+        } while (arr[j] > pivot);
+
+        if (i >= j)
+            return j;
+
+        swap(&arr[i], &arr[j]);
     }
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[high];
-    arr[high] = temp;
-    
-    return (i + 1);
 }
 
 void quickSort(int arr[], int low, int high) {
     if (low < high) {
-        int pi = partition(arr, low, high);
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
+        int p = partition(arr, low, high);
+
+        quickSort(arr, low, p);
+        quickSort(arr, p + 1, high);
     }
 }
 
